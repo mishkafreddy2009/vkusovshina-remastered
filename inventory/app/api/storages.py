0ceb9322e47033dcf595304bb7fb2c99517730fd
Timofey Basuev <echiauta@gmail.com>
2024-07-28 00:35:32 +0500

    feat(storages): add not found handler to storage products endpoint

--- inventory/app/api/storages.py
+++ inventory/app/api/storages.py
@@ -10,34 +10,43 @@ router = APIRouter(prefix="/storages", tags=["storages"])
 
 
 @router.get("/", response_model=list[StoragesPublic])
-def read_storages(offset: int = 0, limit: int = 100, session: Session = Depends(get_session)) -> Sequence[Storage]:
+def read_storages(
+    offset: int = 0, limit: int = 100, session: Session = Depends(get_session)
+) -> Sequence[Storage]:
     storages = session.exec(select(Storage).offset(offset).limit(limit)).all()
     return storages
 
 
 @router.get("/{storage_id}/", response_model=StoragePublic)
-def read_storage(storage_id: int, session: Session = Depends(get_session)) -> Storage | None:
+def read_storage(
+    storage_id: int, session: Session = Depends(get_session)
+) -> Storage | None:
     storage = session.get(Storage, storage_id)
     if not storage:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"storage with id {storage_id} not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"storage with id {storage_id} not found",
+        )
     return storage
 
 
 @router.post("/", response_model=StoragePublic, status_code=status.HTTP_201_CREATED)
-def create_storage(storage_in: StorageCreate, session: Session = Depends(get_session)) -> Storage:
+def create_storage(
+    storage_in: StorageCreate, session: Session = Depends(get_session)
+) -> Storage:
     db_storage = Storage.model_validate(storage_in)
     session.add(db_storage)
     session.commit()
     session.refresh(db_storage)
     return db_storage
-    # storage_obj = Storage(**storage_in.model_dump())
-    # session.add(storage_obj)
-    # session.commit()
-    # session.refresh(storage_obj)
-    # return storage_obj
 
 
 @router.get("/{storage_id}/products")
 def read_storage_products(storage_id: int, session: Session = Depends(get_session)):
     storage = session.get(Storage, storage_id)
+    if not storage:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="storage with given id not found",
+        )
     return storage.products
